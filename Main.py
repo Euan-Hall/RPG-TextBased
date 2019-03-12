95f0a3372c05832fb327dfb430074cf9e4cd5171
Euan Hall <37418971+Euan-Hall@users.noreply.github.com>
2019-03-12 16:10:12 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,27 +13,33 @@ Welcome to the help menu for PyPg
 
 
 def menu():
-    print("""Welcome to a Python, text-based RPG Game - PyPG
+    menu = """
+Welcome to a Python, text-based RPG Game - PyPG
 -----------------------------------------------
 1. Start			Make sure your choice is 
 2. Help				the number. I.e. 1 for start.	
-3. Exit				Game Created by Euan Hall""")
+3. Exit				Game Created by Euan Hall
 
-    try:
-        choice = int(input("Choice:"))
-    except TypeError:
-        choice = 0
+"""
     
+
     startDone = False
     while startDone == False:
+        os.system('cls')
+        print(menu)
+        try:
+            choice = int(input("Choice:"))
+        except TypeError:
+            choice = 0
+        
         if choice == 1: # Start Game
             startDone == True
-            os.cls()
+            os.system("cls")
             start()
             break
             
         elif choice == 2: # Chose Help menu.
-            os.cls()
+            os.system("cls")
             gameHelp()
             break
             
@@ -41,7 +47,7 @@ def menu():
             exit()
 
         else: # Continue the loop if statement not met.
-            pass
+            continue
         
 
 menu()
